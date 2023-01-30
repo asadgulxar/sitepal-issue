0ff00008e60386166a98306a8cb5bef56773b6e9
Asad Gulzar <asadgulxar10@gmail.com>
2023-01-30 16:25:48 +0500

    sitepal-issue

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import { SitePal } from "sitepal-react";
 
 function App() {
+  const [showSitepal, setShowSitepal] = useState(true);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={() => setShowSitepal((val) => !val)}>
+        {showSitepal ? "unmount" : "mount"}
+      </button>
+      {showSitepal && (
+        <SitePal embed='8392872,200,200,"",1,1,2747681,0,1,0,"KM1eb6QyXGYolnCUoJqbp2f41T5IULrt",0,"256|480|15|15|L|B|false|0|0|0|0|vhss-d.oddcast.com/admin/img//no_image.jpg|Double%20click%20to%20play%20me|0|C|0|0|0|0"' />
+      )}
     </div>
   );
 }
